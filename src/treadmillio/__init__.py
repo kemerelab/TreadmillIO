823a45ac76739084574c2bf02505d77792a437db
Caleb Kemere <ckemere@gmail.com>
2024-06-12 14:17:10 -0400

    Finally seem to have fixed installation issues

--- src/treadmillio/__init__.py
+++
@@ -1,6 +0,0 @@
-#from .serialinterface import SerialInterface
-#from .soundstimulus import SoundStimulus
-#from .rewardzone import RewardZoneController
-# from .rendertrack import RenderTrack
-#from .taskstatemachine import TaskStateMachine
-# from .serialinterfacesimulator
